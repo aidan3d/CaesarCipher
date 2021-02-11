af3613a285fb15861753d8854050cffa3c3ade4c
Aidan Hegarty <aidanhegarty@yahoo.com>
2021-02-10 23:58:31 -0500

    Initial commit

---
+++ src/com/aidan3d/CaesarCipher.java
@@ -0,0 +1,12 @@
+package com.aidan3d;
+import edu.duke.*;
+
+public class CaesarCipher {
+    public String encrypt() {
+        String returnStatement = ""; // I am using this as a little "something"
+                                     // to hold another "something" to send back!
+
+        // Whisper a "sweet nothing" to the calling method.
+        return returnStatement;
+    };
+}
---
+++ src/com/aidan3d/Main.java
@@ -0,0 +1,8 @@
+package com.aidan3d;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello, world.");
+    }
+}
