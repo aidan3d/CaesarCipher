517813cf87f780755a045efb97170701d9fdd064
Aidan Hegarty <aidanhegarty@yahoo.com>
2021-02-11 00:01:32 -0500

    Roll out the 'welcome mat!'

--- src/com/aidan3d/Main.java
+++ src/com/aidan3d/Main.java
@@ -3,6 +3,7 @@ package com.aidan3d;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello, world.");
+        // Roll out the 'welcome mat!'
+        System.out.println("Hello, world.");
     }
 }
