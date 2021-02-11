44a01e68d4ee735032670b8bc133030a8a9b2622
Aidan Hegarty <aidanhegarty@yahoo.com>
2021-02-11 17:25:22 -0500

    Further comment-cleaning

--- src/com/aidan3d/CaesarCipher.java
+++ src/com/aidan3d/CaesarCipher.java
@@ -2,7 +2,7 @@ package com.aidan3d;
 import edu.duke.*;
 
 /****
- *  This program encodes a message.
+ * This program encodes a message.
  *
  * @author Aidan Hegarty (with a great deal of help from Duke University via Coursera!)
  * @version 2/11/21
@@ -10,11 +10,12 @@ import edu.duke.*;
 public class CaesarCipher {
     /**
      * The "engine" of the program—this method does the actual leg-work (as all engines do!)!
-     * @param input  The message we wish to encode
-     * @param key    The number of spaces we want to move the letters of the alphabet, i.e., "3" moves "a" to "d," etc.
+     * @param input The message we wish to encode
+     * @param key The number of spaces we want to move the letters of the alphabet, i.e., "3" moves "a" to "d," etc.
      * @return An encrypted message
      */
     public String encrypt(String input, int key) {
         // The code will be typed in here.
+        return input;
     }
 }
