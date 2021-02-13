41bdbd7ec16b18b9f6cc5afb573ee3cfdb3a2e7c
Aidan Hegarty <aidanhegarty@yahoo.com>
2021-02-12 22:29:32 -0500

    Working inside encrypt()

--- src/com/aidan3d/CaesarCipher.java
+++ src/com/aidan3d/CaesarCipher.java
@@ -5,18 +5,23 @@ import edu.duke.*;
  * This program encodes a message.
  *
  * @author Aidan Hegarty (with a great deal of help from Duke University via Coursera!)
- * @version 2/11/21
+ * @version 2/12/21
  */
 
 
 public class CaesarCipher {
 
     /***
-     * Checks whether a character is sitting in the seat (i.e., whether a specific chacter is
-     * "sitting on the 'bus'!"
-     * @param currentPersonOfInterest The current character being looked at
-     * @param alphabets Basically, our solution set of available characters to encode. We can encode those on this list
-     * @return A signal flag, rasied if a chracter is to be found in the provided lower- and upper-case "alphabets" string
+     * Checks whether a character is sitting in the seat
+     * (i.e., whether a specific character is "sitting on
+     * the 'bus'!")
+     * @param currentPersonOfInterest   The current character being looked at
+     * @param alphabets                 Basically, our solution set of available
+     *                                  characters to encode. We can encode those
+     *                                  on this list
+     * @return                          A signal flag, raised if a character is
+     *                                  to be found in the provided lower- and
+     *                                  upper-case "alphabets" string
      */
     private boolean isHuman(char currentPersonOfInterest, String alphabets) {
 
@@ -34,24 +39,33 @@ public class CaesarCipher {
 
 
     /**
-     * The "engine" of the program—this method does the actual leg-work (as all engines do!)!
-     * @param bus The message we wish to encode. Thought of as a "bus" carrying "passengers" (letters of the alphabet)
-     * @param seatsMoved The number of spaces we want to move the letters of the alphabet, i.e., "3" moves "a" to "d," etc.
-     *                   The analogy here is that we "move" passengers in a single row of "seats" up by a certain number of
-     *                   those seats—we "slide" them up, if you will
-     * @return An encrypted message
+     * The "engine" of the program—this method does the actual leg-work
+     * (as all engines do!)!
+     * @param bus           The message we wish to encode—I think of this
+     *                      as a "bus" carrying "passengers" (letters of
+     *                      the alphabet).
+     * @param seatsMoved    The number of spaces we want to move the letters
+     *                      of the alphabet, i.e., "3" moves "a" to "d," etc.
+     *                      The analogy here is that we "move" passengers in
+     *                      a single row of "seats" up by a certain number
+     *                      of those seats—we "slide" them up, if you will
+     * @return              An encrypted message
      */
     public String encrypt( String passengers, int seatsMoved ) {
 
         // First, I'll create a kind of ship's manifest of those I
         // expect to be seated on my "bus..."
-        String IDENTITIES = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";  // "People" we're looking for
+        String EXISTING_SEATING_LIST =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";  // "People" we're looking for
+
+        // String REVISED_SEATING_LIST = adjustSeatingPlan(EXISTING_SEATING_LIST, seatsMoved);
+
 
         // ...now, let's create an empty bus!
         StringBuilder bus = new StringBuilder( passengers );
 
         // Create a character variable to reference the
-        // current "seat" being looked at (all seats have
+        // current "seat" being looked at (all seats dhave
         // a passenger of some kind, and are envisioned to
         // be, abstractly, placed in a single row. Cool bus!
         char seat;
@@ -71,8 +85,8 @@ public class CaesarCipher {
             // Look at the current "seat."
             seat = bus.charAt(i);
 
-            if ( isHuman( seat, IDENTITIES ) ) {
-                // bus.charAt(i) = alter(seat) = seat;
+            if ( isHuman( seat, EXISTING_SEATING_LIST ) ) {
+                // bus.charAt(i) = alter(seat) = seaft;
             } // End of the single-alternative
               // decision structure
         } // End of the counter-controlled loop
